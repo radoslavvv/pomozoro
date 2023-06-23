7fb974a141dd91118526cfff1b0ca44bc5386562
Radoslav <radoslav.varadinov@gmail.com>
2023-06-23 16:56:17 +0300

    Changed project build command

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,6 @@ import styles from "./App.module.scss";
 import Clock from "./clock/Clock";
 import ModeSlider from "./modeSlider/ModeSlider";
 import Options from "./options/Options";
-import ProgressBar from "./clock/progressBar/ProgressBar";
 
 function App() {
 	return (
--- src/components/clock/progressBar/ProgressBar.tsx
+++ src/components/clock/progressBar/ProgressBar.tsx
@@ -6,17 +6,31 @@ import { RootState, useAppDispatch } from "../../../store/Store";
 import { useSelector } from "react-redux";
 
 const ProgressBar = (props: IProgressBarProps) => {
-  const progressBarValue: any = useSelector((state: RootState) => state.clock.progressBarValue);
+	const progressBarValue: any = useSelector(
+		(state: RootState) => state.clock.progressBarValue
+	);
 
-  return (
-    <>
-      <div className={styles.progressBar}>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className={styles.circle}>
-          {progressBarValue < 905 && <circle cx="175" cy="175" r="145" strokeLinecap="round" style={{ strokeDashoffset: progressBarValue }}></circle>}
-        </svg>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.progressBar}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					version="1.1"
+					className={styles.circle}
+				>
+					{progressBarValue < 905 && (
+						<circle
+							cx="175"
+							cy="175"
+							r="145"
+							strokeLinecap="round"
+							style={{ strokeDashoffset: progressBarValue }}
+						></circle>
+					)}
+				</svg>
+			</div>
+		</>
+	);
 };
 
 export default ProgressBar;
